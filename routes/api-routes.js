7e61e3dfb7e9d5ed5438f05480cfb802d661232b
Jose Orozco <jose816orozco@gmail.com>
2021-05-26 20:18:23 -0500

    created API get request

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,9 +2,16 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const util = require("util");
+const path = require("path");
+// router.get("/notes", (req, res) => {
+//   fs.readFileSync("../db/db.json", "utf8").then((notes) => res.json(notes));
+// });
 
 router.get("/notes", (req, res) => {
-  fs.readFileSync("../db/db.json", "utf8").then((notes) => res.json(notes));
+  let data = fs.readFileSync(path.resolve(__dirname, "../db/db.json"), "utf8");
+  console.log(data);
+  res.json(data);
+  // res.send('hello');
 });
 
 module.exports = router;
