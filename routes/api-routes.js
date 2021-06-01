6a0ff368b636c11ce98fc1938f7bcc15b462c1d3
Jose Orozco <jose816orozco@gmail.com>
2021-05-31 20:24:25 -0500

    editing route post part 2

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -31,48 +31,47 @@ router.get("/notes", (req, res) => {
 });
 
 // router.post("/notes", (req, res) => {
-//   fs.writeFileSync(
-//     path.join(__dirname, "db/db.json"),
-//     JSON.stringify(arrayOfNotes),
-//     (err) => {
-//       if (err) {
-//         return err;
-//       }
-//       console.log(arrayOfNotes);
-//     }
-//   );
-//   res.send("success");
-// });
+//     console.log("i hit the route!");
+//    console.log(req.body);
+//    console.log(arrayOfNotes)//so you can make sure you are getting the right object
+//     fs.writeFileSync(
+//       path.join(__dirname, "db/db.json"),
+//       JSON.stringify(arrayOfNotes),
+//       (err) => {
+//         if (err) {
+//           throw err.message;
+//         }
 
-// router.post("/notes", (req, res) => {
-//   console.log("i hit the route!");
-//   fs.writeFileSync(
-//     path.join(__dirname, "db/db.json"),
-//     JSON.stringify(arrayOfNotes),
-//     (err) => {
-//       if (err) {
-//         return err;
-//       }
-//       console.log(arrayOfNotes);
-//     }
-//   );
-//   res.send("success");
-// });
+//       });
+//     res.send("success");
+//    });
 
 router.post("/notes", (req, res) => {
-    console.log("i hit the route!");
-   console.log(req.body);
-   console.log(arrayOfNotes)//so you can make sure you are getting the right object
+  fs.readFile(path.join(__dirname, "db/db.json"), (err, data) => {
+    if (err) {
+      return err;
+    }
+    let arrayOfNotes = JSON.parse(data);
+
+    const note = {
+      title: req.body.title,
+      text: req.body.text,
+    };
+
+    arrayOfNotes.push(note);
+    console.log(arrayOfNotes);
     fs.writeFileSync(
       path.join(__dirname, "db/db.json"),
       JSON.stringify(arrayOfNotes),
       (err) => {
         if (err) {
-          throw err.message;
+          return err;
         }
-       
-      });
+        console.log(arrayOfNotes);
+      }
+    );
     res.send("success");
-   });
+  });
+});
 
 module.exports = router;
